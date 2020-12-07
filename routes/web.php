5833355a074f05c1f1958fbde063c39512bf3b78
unknown <yuni-messi19@hotmail.com>
2020-12-07 17:42:37 +0100

    first commit3

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
-Route::resource('enviados', 'App\Http\Controllers\EnviarController');
+Route::resource('enviados', 'EnviarController');
 Route::resource('empleados', 'EmpleadoController');
 Route::resource('clientes', 'ClienteController');
 Route::resource('ordenes', 'OrdenController');
